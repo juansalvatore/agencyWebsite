71e36c5d85f8d9f9f3bcbf14cc83e2ddbcf16624
Juan Salvatore <juansalvatore@live.com.ar>
2017-10-24 14:05:29 -0300

    Create landing

---
+++ landing.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sudco</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:800" rel="stylesheet">
+    <link rel="stylesheet" href="styles/landing-style.min.css">
+  </head>
+  <body>
+
+    <div class="marco">
+      <div class="contenedor">
+
+      </div>
+      <img src="HE LLO.png" alt=""><span class="puntoAmarillo"></span>
+    </div>
+  </body>
+</html>
