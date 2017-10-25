3c8cbdbd851bb0fa04a80e867ee7f5db8f2135ef
Juan Salvatore <juansalvatore@live.com.ar>
2017-10-25 01:36:34 -0300

    Correct landing

--- landing.php
+++ landing.php
@@ -3,16 +3,34 @@
   <head>
     <meta charset="utf-8">
     <title>Sudco</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link href="https://fonts.googleapis.com/css?family=Montserrat:800" rel="stylesheet">
     <link rel="stylesheet" href="styles/landing-style.min.css">
   </head>
   <body>
+    <div class="menu">
 
-    <div class="marco">
-      <div class="contenedor">
+    </div>
+    <div class="header">
+
+    </div>
+    <div class="redes">
+
+    </div>
+    <div class="bottom">
+
+    </div>
+    <div class="container">
+      <img src="HE LLO.png" alt="">
+      <div class="yellowBar">
 
       </div>
-      <img src="HE LLO.png" alt=""><span class="puntoAmarillo"></span>
     </div>
+
+    <div class="infoContainer">
+
+    </div>
+
   </body>
 </html>
